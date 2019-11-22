4a42ce8354ee3ee76e34b99fad4f63709d850d55
Luis Cazzullo <lcazzullo@gmail.com>
2019-11-21 23:29:07 -0300

    Index and main css were added

---
+++ src/index.js
@@ -0,0 +1,4 @@
+//Dependiendo de las configuraciones iniciales, habrá muchas formas de importar librerías.
+import '../node_modules/jquery/dist/jquery';
+import '../node_modules/popper.js/dist/popper';
+import '../node_modules/bootstrap/dist/js/bootstrap';
